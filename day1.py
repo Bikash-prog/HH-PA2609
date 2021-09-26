04e86a4ede719c941ce153fe29b4cc0891ff7a2b
vikaskhullar <vikas.khullar@gmail.com>
2021-09-26 09:50:50 +0530

    update

--- day1.py
+++ day1.py
@@ -5,11 +5,54 @@ Created on Sat Sep 25 20:21:51 2021
 @author: vikas
 """
 
+#string by default
+
+print()
+print("Welcome")
+print("To Python")
+
+
+print ("Welcome", "to", "Python")
+print ("Welcome", "to", "Python", sep='0')
+
+
+print ("Welcome", "to", "Python", end='-----')
+print ("Welcome", "to", "Python", sep='0', end='-----')
+print ("Welcome", "to", "Python", sep='0')
+
+print?
+help(print)
+
+
 import pandas
 
-a= 10
-b= 20
+help(pandas)
 
-print("Welcom")
 
-d=a+b
+import pandas as pd
+
+help(pd)
+
+help(pandas)
+
+
+import pandas
+
+pandas.__version__
+
+import pandas as pd
+pd.__version__
+
+
+!pip install pandas
+!conda install pandas
+
+
+
+
+
+
+
+
+
+
